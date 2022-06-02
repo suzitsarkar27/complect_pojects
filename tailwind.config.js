3af2dcc55388e2cada8a296c88c5dcec207fcd8f
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-02 22:29:48 +0600

    navbar_start

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#176F6B",
+          secondary: "#FFC000",
+          accent: "#FFEDD5",
+          neutral: "#F3F4F6",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
