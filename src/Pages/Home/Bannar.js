caa535a2c5156c538de7a64cbc650b3cf91c78cb
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-04 13:59:17 +0600

    bannar

--- src/Pages/Home/Bannar.js
+++ src/Pages/Home/Bannar.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Bannar = () => {
     return (
-        <div class="hero h-[60vh] bg-accent mt-1 ">
+        <div class="hero h-screen lg:h-[60vh] bg-accent mt-1 ">
         <div class="hero-content flex-col lg:flex-row">
         
           <div>
-            <h1 class="text-5xl font-bold">Box Office News!</h1>
-            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <p className='text-xl'>Best Qulity</p>
+            <h1 class="text-4xl font-bold">Professional Cleaning Services</h1>
+            <p class="py-6 max-w-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
             <button class="btn btn-primary">Get Started</button>
           </div>
-          <img src="https://i.ibb.co/0VVL1c5/bucketgirl.png" className='h-full p-x-5' />
+          <div className='h-[60vh] shrink-0'>
+             <img src="https://i.ibb.co/0VVL1c5/bucketgirl.png" className='h-full ' />
+          </div>
         </div>
       </div>
     );
